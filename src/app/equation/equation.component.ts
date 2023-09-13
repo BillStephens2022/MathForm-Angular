9c75f9eb51c77465ef2ae497a6f44ad88da24d3f
BillStephens2022 <two4onebill@yahoo.com>
2023-09-13 15:42:00 -0400

    updated equation component with form and a custom form control

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  }, [
+    (form: AbstractControl) => {
+      const { a, b, answer } = form.value;
+      if (a + b === parseInt(answer)) {
+        return null;
+      }
+      return { addition: true }
+    }
+  ]);
 
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
