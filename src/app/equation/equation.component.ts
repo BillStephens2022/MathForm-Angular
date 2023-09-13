5e02be971267e540d73b4bf99d9214eb29396519
BillStephens2022 <two4onebill@yahoo.com>
2023-09-13 16:30:01 -0400

    added a statistic showing the seconds per solution

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { delay, filter, scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -8,6 +9,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent {
+  secondsPerSolution = 0;
   mathForm = new FormGroup(
     {
       a: new FormControl(this.randomNumber()),
@@ -23,6 +25,31 @@ export class EquationComponent {
 
   get b() {
     return this.mathForm.value.b;
+  }
+
+  ngOnInit() {
+    // added delay (imported from rxjs/operators) so that user can see the answer they
+    // type in briefly (100ms) before the next values show.
+    this.mathForm.statusChanges
+      .pipe(
+        filter((value) => value === 'VALID'),
+        delay(100),
+        scan((acc, value) => {
+          return {
+            numberSolved: acc.numberSolved + 1,
+            startTime: acc.startTime
+          }
+        }, { numberSolved: 0, startTime: new Date() })
+      )
+      .subscribe(({ numberSolved, startTime }) => {
+        this.secondsPerSolution =
+          (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: '',
+        });
+      });
   }
 
   randomNumber() {
